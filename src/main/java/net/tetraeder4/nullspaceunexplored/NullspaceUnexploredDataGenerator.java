601b02c1430d5e07a7008d2708fbc8a2b942ede8
tetraeder4 <tetraeder4@user.noreply.github>
2026-08-28 00:03:08 +0200

    added ModRecipeProvider and added 3 new recipes

--- src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexploredDataGenerator.java
+++ src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexploredDataGenerator.java
@@ -3,6 +3,7 @@ package net.tetraeder4.nullspaceunexplored;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.tetraeder4.nullspaceunexplored.datagen.ModModelProvider;
+import net.tetraeder4.nullspaceunexplored.datagen.ModRecipeProvider;
 
 public class NullspaceUnexploredDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -10,5 +11,6 @@ public class NullspaceUnexploredDataGenerator implements DataGeneratorEntrypoint
 		var pack = fabricDataGenerator.createPack();
 
 		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
---
+++ src/main/java/net/tetraeder4/nullspaceunexplored/datagen/ModRecipeProvider.java
@@ -0,0 +1,56 @@
+package net.tetraeder4.nullspaceunexplored.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.CookingBookCategory;
+import net.minecraft.world.level.ItemLike;
+import net.tetraeder4.nullspaceunexplored.block.ModBlocks;
+import net.tetraeder4.nullspaceunexplored.item.ModItems;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeProvider createRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+        return new RecipeProvider(registries, output) {
+            @Override
+            public void buildRecipes() {
+                //also works with lists
+                oreSmelting(List.of(ModBlocks.BACKROOMS_CARPET_BLOCK), RecipeCategory.MISC, CookingBookCategory.BLOCKS, Items.YELLOW_WOOL, 0.1f, 200, "Backrooms");
+                //just fyi: nineBlockStorageRecipes(RecipeCategory.MISC, ModItems.DRYWALL_DEBRIS, RecipeCategory.BUILDING_BLOCKS, ModBlocks.BACKROOMS_WALL_BLOCK);
+
+                shaped(RecipeCategory.MISC, ModBlocks.BACKROOMS_CARPET_BLOCK, 8)
+                        .pattern("WWW")
+                        .pattern("WBW")
+                        .pattern("WWW")
+                        .define('W', Items.YELLOW_WOOL)
+                        .define('B', Items.WATER_BUCKET)
+                        .unlockedBy(getHasName(ModBlocks.BACKROOMS_CARPET_BLOCK), has(ModBlocks.BACKROOMS_CARPET_BLOCK))
+                        .group("Backrooms").save(output);
+
+                shaped(RecipeCategory.MISC, ModBlocks.BACKROOMS_WALL_BLOCK, 1)
+                        .pattern("DD")
+                        .pattern("DD")
+                        .define('D', ModItems.DRYWALL_DEBRIS)
+                        .unlockedBy(getHasName(ModItems.DRYWALL_DEBRIS), has(ModItems.DRYWALL_DEBRIS))
+                        .group("Backrooms").save(output);
+
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "Nullspace Unexplored Recipes";
+    }
+}
