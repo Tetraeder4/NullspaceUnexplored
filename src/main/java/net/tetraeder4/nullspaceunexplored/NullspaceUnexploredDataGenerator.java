296615921418c429380bc9a499f5b5c8e22c2bc0
tetraeder4 <tetraeder4@user.noreply.github>
2026-08-27 18:16:23 +0200

    added Model generator (overides all manual changes in generated package

--- src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexploredDataGenerator.java
+++ src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexploredDataGenerator.java
@@ -2,10 +2,13 @@ package net.tetraeder4.nullspaceunexplored;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.tetraeder4.nullspaceunexplored.datagen.ModModelProvider;
 
 public class NullspaceUnexploredDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
--- src/main/java/net/tetraeder4/nullspaceunexplored/item/ModItems.java
+++ src/main/java/net/tetraeder4/nullspaceunexplored/item/ModItems.java
@@ -23,9 +23,9 @@ public class ModItems {
     public static void registerModItems() {
         NullspaceUnexplored.LOGGER.info("Registering Mod Items for " + NullspaceUnexplored.MOD_ID);
 
-        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS).register(output -> {
-            output.accept(DRYWALL_DEBRIS);
-        });
+        //CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS).register(output -> {
+        //    output.accept(DRYWALL_DEBRIS);
+        //});
     }
 }
 
