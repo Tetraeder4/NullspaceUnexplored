b7fa8acc7d6c2350c6a783a2f7f318cf56f68995
tetraeder4 <tetraeder4@user.noreply.github>
2026-08-27 16:25:13 +0200

    updated creative mode tabs to also support items

--- src/main/java/net/tetraeder4/nullspaceunexplored/creativemodetab/ModCreativeModeTabs.java
+++ src/main/java/net/tetraeder4/nullspaceunexplored/creativemodetab/ModCreativeModeTabs.java
@@ -3,7 +3,7 @@ package net.tetraeder4.nullspaceunexplored.creativemodetab;
 import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
 import net.tetraeder4.nullspaceunexplored.NullspaceUnexplored;
 import net.tetraeder4.nullspaceunexplored.block.ModBlocks;
-//import net.tetraeder4.nullspaceunexplored.item.ModItems;
+import net.tetraeder4.nullspaceunexplored.item.ModItems;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
@@ -13,16 +13,16 @@ import net.minecraft.world.item.ItemStack;
 
 
 public class ModCreativeModeTabs {
-    /*
-        public static final CreativeModeTab FLUORITE_ITEM_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
-                Identifier.fromNamespaceAndPath(TutorialMod.MOD_ID, "fluorite_items"),
-                FabricCreativeModeTab.builder().icon(() -> new ItemStack(ModItems.FLUORITE))
-                        .title(Component.translatable("creativemodetab.tutorialmod.fluorite_items"))
-                        .displayItems((parameters, output) -> {
-                            output.accept(ModItems.FLUORITE);
-                            output.accept(ModItems.RAW_FLUORITE);
-                        }).build());
-    */
+
+    public static final CreativeModeTab FLUORITE_ITEM_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
+            Identifier.fromNamespaceAndPath(NullspaceUnexplored.MOD_ID, "backrooms_items"),
+            FabricCreativeModeTab.builder().icon(() -> new ItemStack(ModItems.DRYWALL_DEBRIS))
+                    .title(Component.translatable("creativemodetab.tutorialmod.backrooms_items"))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.DRYWALL_DEBRIS);
+
+                    }).build());
+
 
     public static final CreativeModeTab BACKROOMS_BLOCK_TAB = Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB,
             Identifier.fromNamespaceAndPath(NullspaceUnexplored.MOD_ID, "backrooms_blocks"),
