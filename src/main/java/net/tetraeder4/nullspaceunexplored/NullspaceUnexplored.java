49eb34828ccfacf095ce753952d3f40ad4c91ea7
tetraeder4 <tetraeder4@user.noreply.github>
2026-08-27 12:48:05 +0200

    removed test comment

--- src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexplored.java
+++ src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexplored.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 public class NullspaceUnexplored implements ModInitializer {
 	public static final String MOD_ID = "nullspaceunexplored";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-//test
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
