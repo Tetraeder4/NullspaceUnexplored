04aef0a625b06e0076885976c8f47fdf30823c9b
tetraeder4 <tetraeder4@user.noreply.github>
2026-08-27 12:30:32 +0200

    Initial commit uwu

---
+++ src/main/java/net/tetraeder4/nullspaceunexplored/NullspaceUnexploredDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tetraeder4.nullspaceunexplored;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NullspaceUnexploredDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
