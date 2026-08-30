d5487799a93ca452c673fe2144d43d3b703a7a96
tetraeder4 <tetraeder4@user.noreply.github>
2026-08-30 20:20:53 +0200

    cleaned code up

--- src/main/java/net/tetraeder4/nullspaceunexplored/datagen/ModBlockLootTableProvider.java
+++ src/main/java/net/tetraeder4/nullspaceunexplored/datagen/ModBlockLootTableProvider.java
@@ -1,19 +1,12 @@
 package net.tetraeder4.nullspaceunexplored.datagen;
 
-import com.sun.jna.platform.win32.Tlhelp32;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootSubProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
-import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.tetraeder4.nullspaceunexplored.block.ModBlocks;
@@ -47,13 +40,8 @@ public class ModBlockLootTableProvider extends FabricBlockLootSubProvider {
     }
 
     public LootTable.Builder createMultipleDrops(final Block block, Item item, float minDrops, float maxDrops) {
-        HolderLookup.RegistryLookup<Enchantment> enchantments = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
-
-        return this.createSilkTouchDispatchTable(block, (LootPoolEntryContainer.Builder)this.applyExplosionDecay
+        return this.createSilkTouchDispatchTable(block, this.applyExplosionDecay
                 (block, LootItem.lootTableItem(item)
                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))));
-
-                        // not needed in this case to stop infinite rescources
-                        //.apply(ApplyBonusCount.addOreBonusCount(enchantments.getOrThrow(Enchantments.FORTUNE)))));
     }
 }
